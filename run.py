023098d52d85230c6dec4d0c3d1d3d7dd0adc10a
minhducit <minhducit@gmail.com>
2019-10-15 09:16:37 +0200

    Re-organize the class

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from src.application import app
 import logging
 import kafka
-from kafka import KafkaConsumer, KafkaProducer
-import threading
+from src.kafka_operations.KafkaPythonConsumer import KafkaPythonConsumer
 
 # setup logging configuration
 # Create a new handler for log messages that will send them to standard error
@@ -14,38 +13,8 @@ formatter = logging.Formatter(log_format)
 handler.setFormatter(formatter)
 handler.setLevel(logging.DEBUG)
 
-class KafkaConsumerThread(object):
-    """ Threading example class
-    The run() method will be started and it will run in the background
-    until the application exits.
-    """
-
-    def __init__(self, interval=1):
-        """ Constructor
-        :type interval: int
-        :param interval: Check interval, in seconds
-        """
-        self.interval = interval
-
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                                  # Start the execution
-
-    def run(self):
-        """ Method that runs forever """
-        consumer = KafkaConsumer(bootstrap_servers='127.0.0.1:9092',
-                             auto_offset_reset='latest',
-                             group_id=None)
-        consumer.subscribe(['test_queue1'])
-        for msg in consumer:
-            print("Message retrieved")
-            if msg is None:
-                continue
-            print(msg.value.decode("utf-8"))
-
-
 
 if __name__ == '__main__':
-    example = KafkaConsumerThread()
+    consumer = KafkaPythonConsumer(topic = 'generic_queue')
     app.run(port = 8765, debug=True, use_reloader=False, threaded=False)
 
---
+++ src/kafka_operations/KafkaPythonConsumer.py
@@ -0,0 +1,32 @@
+import threading
+from kafka import KafkaConsumer
+
+class KafkaPythonConsumer(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
+
+    def __init__(self, topic = None, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
+        self.topic = topic
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        """ Method that runs forever """
+        consumer = KafkaConsumer(bootstrap_servers='127.0.0.1:9092',
+                             auto_offset_reset='latest',
+                             group_id=None)
+        consumer.subscribe([self.topic])
+        for msg in consumer:
+            print("Message retrieved")
+            if msg is None:
+                continue
+            print(msg.value.decode("utf-8"))
